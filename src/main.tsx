dfae38ab255a9b9bf2935fe73ca24176e2dca969
Chris Jones <chris.charles.jones@gmail.com>
2024-11-07 12:39:54 -0800

    added logo files and google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
 import ReactDOM from 'react-dom/client';
+import ReactGA from 'react-ga4';
 import App from './App';
+
+ReactGA.initialize('G-WDC00BWNFK');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,20 @@
-import { createTheme } from '@mantine/core';
+import { createTheme, MantineColorsTuple } from '@mantine/core';
+
+const redWood: MantineColorsTuple = [
+  '#fdf3ec',
+  '#f7e4d8',
+  '#f1c7ab',
+  '#eda779',
+  '#e98c50',
+  '#e77c36',
+  '#e67328',
+  '#cd611d',
+  '#b75617',
+  '#9f480e',
+];
 
 export const theme = createTheme({
-  /** Put your mantine theme override here */
+  colors: {
+    redWood,
+  },
 });
